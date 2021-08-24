6338f33f3a1bf948b41dca4bb382190e7466f392
caballeroagustina <agustinacaballero2001@gmail.com>
2021-08-23 21:29:58 -0300

    entrega 1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,25 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(){
 
+    document.getElementById("submitBtn").addEventListener("click", function () {
+        
+        let inputEmail = document.getElementById("inputEmail");
+        let inputPassword = document.getElementById ("inputPassword") ;
+        let camposCompletos = true; 
+
+        if (inputEmail.value === ""){ 
+            camposCompletos = false;
+        }
+        if(inputPassword.value === ""){
+            camposCompletos = false;
+    
+        }
+        if(camposCompletos){
+            window.location = "inicio.html" ;
+        } else
+       alert ( "Usuario y/o contraseña incorrectos" )
+    }
+    )
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,48 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+var categoriesArray = [];
 
+function showAutos(array){
+
+    let contenido = "";
+    for(let i = 0; i < array.length; i++){
+        let auto = array[i];
+
+        contenido += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + auto.imgSrc + `" alt="` + auto.description + `" class="img-thumbnail">
+                </div>
+               
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ auto.name +`</h4>
+                        <p class="mb-1">` + auto.description + `</p>
+                        <small class="text-muted">` + auto.cost + ` USD</small>
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("listado").innerHTML = contenido;
+    }
+}
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultado){
+        if (resultado.status === "ok")
+        {
+            categoriesArray = resultado.data;
+            //Muestro las categorías ordenadas
+            showAutos(categoriesArray);
+        }
+    });
 });
